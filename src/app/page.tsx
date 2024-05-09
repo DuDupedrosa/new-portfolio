bc78c554044199aeeee27c6b92befd948375f4fc
epedrosa2905 <epedrosa@dxainvest.com>
2024-05-09 20:12:22 -0300

    remove index.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-'use client';
-
 import Header from '@/components/Common/Header';
-import { Button } from '@/components/ui/button';
-import React from 'react';
-import 'react-modern-drawer/dist/index.css';
 
 export default function Home() {
   return (
